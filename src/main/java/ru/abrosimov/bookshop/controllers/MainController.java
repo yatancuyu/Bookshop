9474dbb56056ece8fc6120db203c504f740cba7f
Vladislav Abrosimov <yyatancuyu@yandex.ru>
2022-08-18 22:40:07 +0300

    Add registration & cookies for authorized users

---
+++ src/main/java/ru/abrosimov/bookshop/controllers/MainController.java
@@ -0,0 +1,48 @@
+package ru.abrosimov.bookshop.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.abrosimov.bookshop.dao.BookDAO;
+import ru.abrosimov.bookshop.dao.CustomerDAO;
+import ru.abrosimov.bookshop.models.Customer;
+
+import java.util.Optional;
+
+@Controller
+public class MainController {
+
+    private final BookDAO bookDAO;
+    private final CustomerDAO customerDAO;
+
+    @Autowired
+    public MainController(BookDAO bookDAO, CustomerDAO customerDAO) {
+        this.bookDAO = bookDAO;
+        this.customerDAO = customerDAO;
+    }
+
+    @GetMapping("/")
+    public String showCatalog(@CookieValue(name="login", defaultValue = "NONE") String login,
+                              @CookieValue(name="password", defaultValue = "NONE") String password,
+                              Model model) {
+        boolean authenticated = isAuthenticated(login, password);
+        System.out.println(authenticated);
+        model.addAttribute("auth", authenticated);
+        return "main";
+    }
+
+    private boolean isAuthenticated(String login, String password) {
+        if (login.equals("NONE"))
+            return false;
+
+        Optional<Customer> customerOp = customerDAO.findByLogin(login);
+        if (customerOp.isEmpty())
+            return false;
+
+        Customer customer = customerOp.get();
+        return customer.getPassword().equals(password);
+    }
+
+}
