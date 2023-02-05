c9031b4837029a9cb9aff5744eadea42b5b2dbc4
Vladislav Abrosimov <yyatancuyu@yandex.ru>
2023-02-05 18:58:21 +0300

    Add book creation and editing, also error page

--- src/main/java/ru/abrosimov/bookshop/controllers/BookController.java
+++ src/main/java/ru/abrosimov/bookshop/controllers/BookController.java
@@ -1,5 +1,6 @@
 package ru.abrosimov.bookshop.controllers;
 
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -28,13 +29,12 @@ public class BookController {
         model.addAttribute("auth", customer.isPresent());
         model.addAttribute("admin", customer.isPresent() && customer.get().isAdminRights());
 
-        Optional<Book> book = bookDAO.findById(bookId);
-        if (book.isEmpty()) {
-            model.addAttribute("msg", "Book not found");
-            return "error";
+        String err = checkBook(bookId, model);
+        if (err != null) {
+            return err;
         }
 
-        model.addAttribute("book", book.get());
+        model.addAttribute("book", bookDAO.findById(bookId).get());
         return "book";
     }
 
@@ -42,7 +42,7 @@ public class BookController {
     public String deleteBook(@RequestParam(name = "bookId") Integer bookId,
                              @CookieValue(name = "customerId", defaultValue = "-1") String customerId,
                              Model model) {
-        checkBook(customerId, bookId, model);
+        check(customerId, bookId, model);
 
         bookDAO.delete(bookDAO.findById(bookId).get());
         return "redirect:/";
@@ -52,7 +52,7 @@ public class BookController {
     public String editBook(@RequestParam(name = "bookId") Integer bookId,
                            @CookieValue(name = "customerId", defaultValue = "-1") String customerId,
                            Model model) {
-        String err = checkBook(customerId, bookId, model);
+        String err = check(customerId, bookId, model);
         if (err != null) {
             return err;
         }
@@ -65,7 +65,9 @@ public class BookController {
     public String editBook(@CookieValue(name = "customerId", defaultValue = "-1") String customerId,
                            @ModelAttribute("book") Book book,
                            Model model) {
-        String err = checkBook(customerId, book.getId(), model);
+        System.out.println(book);
+        String err = check(customerId, book.getId(), model);
+
         if (err != null) {
             return err;
         }
@@ -74,15 +76,52 @@ public class BookController {
         return "redirect:/book/" + book.getId();
     }
 
-    private String checkBook(String customerId, Integer bookId, Model model) {
-        Optional<Customer> customer = customerDAO.findById(Integer.parseInt(customerId));
-        if (customer.isEmpty() || !customer.get().isAdminRights()) {
-            model.addAttribute("msg", "Permission denied.");
+    @GetMapping("/book/create")
+    public String createBook(@CookieValue(name = "customerId", defaultValue = "-1") String customerId,
+                             Model model) {
+        String err = checkAdmin(customerId, model);
+        if (err != null) {
+            return err;
+        }
+
+        model.addAttribute("book", new Book());
+        return "editBook";
+    }
+
+    @PostMapping("/book/create")
+    public String createBook(@CookieValue(name = "customerId", defaultValue = "-1") String customerId,
+                           @ModelAttribute("book") Book book,
+                           Model model) {
+        String err = checkAdmin(customerId, model);
+        if (err != null) {
+            return err;
+        }
+
+        bookDAO.save(book);
+        return "redirect:/book/" + book.getId();
+    }
+
+    private @Nullable String check(String customerId, Integer bookId, Model model) {
+        if (checkAdmin(customerId, model) != null || checkBook(bookId, model) != null) {
             return "error";
         }
+        return null;
+    }
+
+    private @Nullable String checkBook(Integer bookId, Model model) {
+        System.out.println(bookId);
         Optional<Book> book = bookDAO.findById(bookId);
         if (book.isEmpty()) {
             model.addAttribute("msg", "Book not found");
+            return "error";
+        }
+        return null;
+    }
+
+    private @Nullable String checkAdmin(String customerId, Model model) {
+        Optional<Customer> customer = customerDAO.findById(Integer.parseInt(customerId));
+        if (customer.isEmpty() || !customer.get().isAdminRights()) {
+            model.addAttribute("msg", "Permission denied.");
             return "error";
         }
         return null;
